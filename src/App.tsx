a02a69d9b5a03d265a2725f8f2e90f19ea61de6f
Edwin Martin <edwin@bitstorm.org>
2024-03-13 15:28:44 +0100

    Amplify quickstart first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import TodoList from "./components/TodoList";
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1>Hello, Amplify 👋</h1>
+            <TodoList />
+        </>
+    );
+}
+
+export default withAuthenticator(App);
