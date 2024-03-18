a6ccf533d14ae1a0d248fc30b6531fc4b7f59a93
Edwin Martin <edwin@bitstorm.org>
2024-03-18 12:37:27 +0100

    Add demo function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import {myDemoFunction} from './functions/my-demo-function/resource'
 
 defineBackend({
   auth,
   data,
+  myDemoFunction,
 });
